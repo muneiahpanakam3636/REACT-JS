955ebf734e1d61cd5a34798500812026e53701e5
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-08-05 10:00:30 +0530

    REACT

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Message from './message/Message'
+let App =()=>{
+    return <div>
+        <h2>App component</h2>
+        <hr/>
+        <Message/>
+        <Message/>
+        <Message/>
+    </div>
+}
+export default App
---
+++ src/message/Message.jsx
@@ -0,0 +1,7 @@
+let Message = ()=>{
+    return <div>
+        <h2>Message Component</h2>
+
+    </div>
+}
+export default Message
