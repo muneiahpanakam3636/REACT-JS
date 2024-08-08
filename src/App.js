6500f98a00521a8fc50ac58b2a8c7eba7f2031fc
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-08-08 09:56:57 +0530

    tst

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 
-
-// import Navbar from './Navbar'
-
-// let App=()=>{
-//     return <div>
-//        <Navbar/>
-//     </div>
-// }
-// export default App;
-
-
-// import CompA from "./props/CompA"
-// let App =()=>{
-//     return <div>
-//         <h2>App Component</h2>
-//         <CompA/>
-//     </div>
-// }
-// export default App;
-
-
-import User from './Component/User';
+import Movie from "./Movies/Movie";
 let App =()=>{
+
     return <div>
         <h2>App component</h2>
-        <hr/>
-        <User/>
+        <Movie/>
+
     </div>
+
 }
 export default App;
